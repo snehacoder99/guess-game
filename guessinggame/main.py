be2d8e6629ba48717a8c8a5581fc3ea511b736f7
snehacoder99 <69866971+snehacoder99@users.noreply.github.com>
2023-06-03 13:32:00 +0530

    Add files via upload

---
+++ guessinggame/main.py
@@ -0,0 +1,19 @@
+import random
+
+def guessing_game():
+    number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = int(input("Enter your guess (1-100): "))
+        attempts += 1
+
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print("Congratulations! You guessed the number in", attempts, "attempts.")
+            break
+
+guessing_game()
